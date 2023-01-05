60023efd3704da21ae05ec8d9a440ba6cf830ded
Chenyang Huang <hchenyang@hotmail.com>
2023-01-05 08:22:57 +0800

    update

--- esm/model/esm2.py
+++ esm/model/esm2.py
@@ -165,7 +165,7 @@ class ESM2(nn.Module):
             log_file.flush()
             idx = torch.tensor(list(range(1, T)))
             perm_idx = torch.randperm(len(idx))
-            rand_idx = idx[perm_idx][:2]
+            rand_idx = idx[perm_idx] # [:2]
             for _i in tqdm(rand_idx):
                 E_old = self.get_energy(tokens)
                 w_0 = tokens[:, _i]
